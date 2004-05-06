957dd5b07b04216597cca27632aacdc08e642870
danilo <>
2004-05-06 16:24:29 +0000

    Add support for translators comments in XML files.

---
+++ tests/results/extract-comments.xml.h
@@ -0,0 +1,10 @@
+char *s = N_("Dum-dee-dum");
+/*  If comment doesn't end on the previous line, it should be handled
+     correctly  */
+char *s = N_("Final thing to translate");
+/*  This comment is not ignored  */
+char *s = N_("First thing to translate");
+/*  This is multi line comment for the following string;
+     second line of multi-line comment  */
+char *s = N_("Something to translate");
+char *s = N_("This doesn't have a comment for translators");
