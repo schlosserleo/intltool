c968a91d9eff079f0191da94ac948e755888a6a4
dobey <>
2004-07-27 15:42:47 +0000

    2004-07-27  Rodney Dawes  <dobey@novell.com>
    	* Makefile.am (SUBDIRS): "." is not a subdir
    
    	* NEWS: Update for 0.31.1
    
    	* configure.in: Change version to 0.31.1

--- autogen.sh
+++ autogen.sh
@@ -4,57 +4,18 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-ORIGDIR=`pwd`
-cd $srcdir
+PKG_NAME="intltool"
+REQUIRED_AUTOMAKE_VERSION="1.6"
 
-PROJECT=intltool
-TEST_TYPE=-f
-FILE=intltoolize.in
-
-DIE=0
-
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have autoconf installed to compile $PROJECT."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-	DIE=1
+(test -f $srcdir/configure.in) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
 }
 
-(automake-1.4 --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have automake installed to compile $PROJECT."
-	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
-	echo "(or a newer version if it is available)"
-	DIE=1
-}
-
-if test "$DIE" -eq 1; then
-	exit 1
-fi
-
-test $TEST_TYPE $FILE || {
-	echo "You must run this script in the top-level $PROJECT directory"
+which gnome-autogen.sh || {
+	echo "You need to install gnome-common from the GNOME CVS"
 	exit 1
 }
 
-if test -z "$*"; then
-	echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
-fi
-
-case $CC in
-*lcc | *lcc\ *) am_opt=--include-deps;;
-esac
-
-aclocal-1.4 $ACLOCAL_FLAGS
-
-automake-1.4 -a $am_opt
-autoconf
-
-cd $ORIGDIR
-
-$srcdir/configure --enable-maintainer-mode "$@"
-
-echo 
-echo "Now type 'make' to build $PROJECT."
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
