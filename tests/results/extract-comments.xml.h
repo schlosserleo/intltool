2458329e0ea78c73295532a111d6afdd6dad942d
danilo <>
2004-08-22 18:50:46 +0000

    2004-08-22  Danilo Å egan  <dsegan@gmx.net>
    	* NEWS: Document additions.
    
    	* intltool-extract.in.in (type_xml): Adjust re to allow for
    	translating both content and attributes (i.e.
    	<_t _name="ah">content</_t>).
    
    	* intltool-extract.in.in (type_xml): Add support for comments
    	before translatable attributes.
    	* tests/cases/extract-comments.xml: Add testcases for comments
    	around attributes.
    	* tests/results/extract-comments.xml.h: Updated results.
    
    	Fixes bug #143581.

--- tests/results/extract-comments.xml.h
+++ tests/results/extract-comments.xml.h
@@ -8,3 +8,10 @@ char *s = N_("First thing to translate");
      second line of multi-line comment  */
 char *s = N_("Something to translate");
 char *s = N_("This doesn't have a comment for translators");
+/*  Comment for *both* attributes and content  */
+char *s = N_("attribute value");
+/*  This comment will appear if attributes can be commented about  */
+char *s = N_("attribute with comment");
+char *s = N_("attribute without comment");
+/*  Comment for *both* attributes and content  */
+char *s = N_("even more content");
