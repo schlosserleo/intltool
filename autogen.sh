98de58a8b267e219347d1fca622c6e7a00c6104d
Rodney Dawes <dobey@gnome.org>
2009-04-26 15:15:06 -0400

    Rename configure.in to the preferred configure.ac 	Update configure script to require new autoconf 	Remove the old/broken spec file 	Remove ChangeLog, NEWS, and MAINTAINERS as they are no longer needed 	Upgrade bzr branch format to 1.9

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ test -z "$srcdir" && srcdir=.
 PKG_NAME="intltool"
 REQUIRED_AUTOMAKE_VERSION="1.6"
 
-(test -f $srcdir/configure.in) || {
+(test -f $srcdir/intltoolize.in) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
