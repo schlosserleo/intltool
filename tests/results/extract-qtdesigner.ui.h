48b2363958fc0047bf671583ae89f7e22c5a7377
David Planella <david.planella@ubuntu.com>
2012-03-12 20:20:59 +0100

    First shot at adding extraction support for Qt Designer .ui files

---
+++ tests/results/extract-qtdesigner.ui.h
@@ -0,0 +1,13 @@
+char *s = N_("Form");
+/* This is a comment (extracomment) */
+char *s = N_("Intltool extract text");
+char *s = N_("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+             "<html><head><meta name=\"qrichtext\" content=\"1\" /><title>HTML document extraction test</title><style type=\"text/css\">\n"
+             "p, li { white-space: pre-wrap; }\n"
+             "</style></head><body style=\" font-family:'Ubuntu'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+             "<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Once upon a time, in a galaxy far, far away... </p></body></html>");
+/* Refers to the action of accepting something */
+char *s = C_("Button", "Ok");
+char *s = C_("WelcomeDialog", "Don't show me this message in the future");
+/* Authentication is required to perform this action */
+char *s = N_("Require authentication");
